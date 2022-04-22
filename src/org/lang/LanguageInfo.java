e6fd07582364bdfe6e16a876ed26c5cdd62e1466
VigneshS02121994 <vigneshitvijay@gmail.com>
2022-04-22 19:21:10 +0530

    added

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,22 @@
+package org.lang;
+
+public class LanguageInfo {
+	public void tamillanguage() {
+		System.out.println("tamil language");
+	}
+	public void englishlanguage() {
+		System.out.println("english language");
+	}
+	public void hindilanguage() {
+		System.out.println("hindi language");
+	}
+	public static void main(String[]args) {
+		LanguageInfo l = new LanguageInfo();
+		l.tamillanguage();
+		l.englishlanguage();
+		l.hindilanguage();
+		StateDetails t = new StateDetails();
+		t.southindia();
+		t.northindia();
+	}
+}
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,15 @@
+package org.lang;
+
+public class StateDetails {
+	public void southindia() {
+		System.out.println("south india");
+	}
+	public void northindia() {
+		System.out.println("north india");
+	}
+	public static void main(String[]args) {
+		StateDetails t = new StateDetails();
+		t.southindia();
+		t.northindia();
+	}
+}
